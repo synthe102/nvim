171b270e428f616bd0d9f510896c65f8be984c13
Léonard Suslian (synthe102) <leonard.suslian@gmail.com>
2023-04-02 22:25:41 +0200

    style: formatting

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -1,166 +1,173 @@
 require("lazy").setup({
-	{
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v2.x",
-		dependencies = {
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" }, -- Required
-			{ "williamboman/mason.nvim" }, -- Optional
-			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
+  {
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v2.x",
+    dependencies = {
+      -- LSP Support
+      { "neovim/nvim-lspconfig" },             -- Required
+      { "williamboman/mason.nvim" },           -- Optional
+      { "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" }, -- Required
-			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
-			{ "hrsh7th/cmp-buffer" }, -- Optional
-			{ "hrsh7th/cmp-path" }, -- Optional
-			{ "saadparwaiz1/cmp_luasnip" }, -- Optional
-			{ "hrsh7th/cmp-nvim-lua" }, -- Optional
+      -- Autocompletion
+      { "hrsh7th/nvim-cmp" },         -- Required
+      { "hrsh7th/cmp-nvim-lsp" },     -- Required
+      { "hrsh7th/cmp-buffer" },       -- Optional
+      { "hrsh7th/cmp-path" },         -- Optional
+      { "saadparwaiz1/cmp_luasnip" }, -- Optional
+      { "hrsh7th/cmp-nvim-lua" },     -- Optional
 
-			-- Snippets
-			{ "L3MON4D3/LuaSnip" }, -- Required
-			{ "rafamadriz/friendly-snippets" }, -- Optional
-			{ "jose-elias-alvarez/null-ls.nvim" },
-			{ "jay-babu/mason-null-ls.nvim" },
-		},
-		config = function()
-			require("user.configs.lsp_zero")
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			require("user.configs.ts")
-		end,
-	},
-	{
-		"simrat39/rust-tools.nvim",
-		dependencies = {
-			{ "neovim/nvim-lspconfig" },
-			{ "nvim-lua/plenary.nvim" },
-			{ "mfussenegger/nvim-dap" },
-		},
-		config = function()
-			require("user.configs.rust_tools")
-		end,
-	},
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v2.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		},
-		config = function()
-			require("user.configs.neo_tree")
-		end,
-	},
-	{
-		"s1n7ax/nvim-window-picker",
-		version = "v1.*",
-		config = function()
-			require("user.configs.nvim_window_picker")
-		end,
-	},
-	{
-		"folke/which-key.nvim",
-		config = function()
-			require("user.configs.which_key")
-		end,
-	},
-	{
-		"windwp/nvim-autopairs",
-		config = function()
-			require("nvim-autopairs").setup()
-		end,
-	},
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		config = function()
-			require("user.configs.catppuccin")
-		end,
-	},
-	{ "mrjones2014/smart-splits.nvim" },
-	{
-		"akinsho/bufferline.nvim",
-		version = "v3.*",
-		dependencies = "nvim-tree/nvim-web-devicons",
-		config = function()
-			require("user.configs.bufferline")
-		end,
-	},
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("user.configs.gitsings")
-		end,
-	},
-	{ "tpope/vim-fugitive" },
-	{
-		"rcarriga/nvim-dap-ui",
-		dependencies = { "mfussenegger/nvim-dap" },
-		config = function()
-			require("user.configs.dapui")
-		end,
-	},
-	{
-		"nvim-telescope/telescope.nvim",
-		version = "0.1.x",
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
-	{
-		"echasnovski/mini.bufremove",
-		version = "*",
-		config = function()
-			require("mini.bufremove").setup()
-		end,
-	},
-	{ "SmiteshP/nvim-navic", dependencies = "neovim/nvim-lspconfig" },
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		config = function()
-			require("indent_blankline").setup()
-		end,
-	},
-	-- {
-	-- 	"folke/noice.nvim",
-	-- 	dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
-	-- 	config = function()
-	-- 		require("user.configs.noice")
-	-- 	end,
-	-- },
-	{ "mbbill/undotree" },
-	{
-		"akinsho/toggleterm.nvim",
-		version = "*",
-		config = function()
-			require("toggleterm").setup()
-		end,
-	},
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("user.configs.lualine")
-		end,
-	},
-	{
-		"numToStr/Comment.nvim",
-		config = function()
-			require("Comment").setup()
-		end,
-	},
-	{
-		"goolord/alpha-nvim",
-		requires = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("user.configs.alpha")
-		end,
-	},
-	{ "jay-babu/mason-nvim-dap.nvim" },
-	{ "rebelot/heirline.nvim" },
-	{ dir = "~/perso/aws_nvim" },
-	{ dir = "~/perso/foo" },
+      -- Snippets
+      { "L3MON4D3/LuaSnip" },             -- Required
+      { "rafamadriz/friendly-snippets" }, -- Optional
+      { "jose-elias-alvarez/null-ls.nvim" },
+      { "jay-babu/mason-null-ls.nvim" },
+    },
+    config = function()
+      require("user.configs.lsp_zero")
+    end,
+  },
+  {
+    "folke/trouble.nvim",
+    requires = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("trouble").setup()
+    end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("user.configs.ts")
+    end,
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    dependencies = {
+      { "neovim/nvim-lspconfig" },
+      { "nvim-lua/plenary.nvim" },
+      { "mfussenegger/nvim-dap" },
+    },
+    config = function()
+      require("user.configs.rust_tools")
+    end,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require("user.configs.neo_tree")
+    end,
+  },
+  {
+    "s1n7ax/nvim-window-picker",
+    version = "v1.*",
+    config = function()
+      require("user.configs.nvim_window_picker")
+    end,
+  },
+  {
+    "folke/which-key.nvim",
+    config = function()
+      require("user.configs.which_key")
+    end,
+  },
+  {
+    "windwp/nvim-autopairs",
+    config = function()
+      require("nvim-autopairs").setup()
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    config = function()
+      require("user.configs.catppuccin")
+    end,
+  },
+  { "mrjones2014/smart-splits.nvim" },
+  {
+    "akinsho/bufferline.nvim",
+    version = "v3.*",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("user.configs.bufferline")
+    end,
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("user.configs.gitsings")
+    end,
+  },
+  { "tpope/vim-fugitive" },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap" },
+    config = function()
+      require("user.configs.dapui")
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    version = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  {
+    "echasnovski/mini.bufremove",
+    version = "*",
+    config = function()
+      require("mini.bufremove").setup()
+    end,
+  },
+  { "SmiteshP/nvim-navic", dependencies = "neovim/nvim-lspconfig" },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      require("indent_blankline").setup()
+    end,
+  },
+  -- {
+  -- 	"folke/noice.nvim",
+  -- 	dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
+  -- 	config = function()
+  -- 		require("user.configs.noice")
+  -- 	end,
+  -- },
+  { "mbbill/undotree" },
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup()
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("user.configs.lualine")
+    end,
+  },
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      require("Comment").setup()
+    end,
+  },
+  {
+    "goolord/alpha-nvim",
+    requires = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("user.configs.alpha")
+    end,
+  },
+  { "jay-babu/mason-nvim-dap.nvim" },
+  { "rebelot/heirline.nvim" },
+  { dir = "~/perso/aws_nvim" },
+  { dir = "~/perso/foo" },
 })
--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -2,71 +2,72 @@ local wk = require("which-key")
 local set = vim.api.nvim_set_keymap
 
 wk.register({
-	["<leader>"] = {
-		g = {
-			name = "Git",
-			s = { ":G<CR>", "Git status" },
-			c = { ":Git commit<CR>", "Git commit" },
-			b = { ":Telescope git_branches<CR>", "Git branches" },
-		},
-		e = { ":Neotree toggle<CR>", "Toggle file tree" },
-		x = { "<cmd>lua MiniBufremove.delete()<CR>", "Close current buffer" },
-		f = {
-			name = "Files",
-			s = { ":Telescope live_grep<CR>", "Search in files using Telescope" },
-			f = { ":Telescope find_files<CR>", "Search files using Telescope" },
-		},
-		l = {
-			name = "LSP",
-			o = { "<cmd>lua vim.diagnostic.open_float()<CR>", "Open LSP diagnostic window" },
-			q = { "<cmd>lua vim.diagnostic.setloclist()<CR>", "Open loclist using split buffer" },
-			l = { ":Telescope loclist<CR>", "Open loclist using Telescope" },
-			d = { "<cmd>lua vim.lsp.buf.definition()<CR>", "Go to definition" },
-			D = { "<cmd>lua vim.lsp.buf.declaration()<CR>", "Go to declaration" },
-			h = {
-				function()
-					vim.lsp.buf.hover()
-				end,
-				"Toggle LSP hover",
-			},
-		},
-		-- h = {
-		-- 	name = "Harpoon",
-		-- 	m = { '<cmd>lua require("harpoon.mark").add_file()<CR>', "Mark file" },
-		-- 	n = { '<cmd>lua require("harpoon.ui").nav_next()<CR>', "Navigate to next file" },
-		-- 	N = { '<cmd>lua require("harpoon.ui").nav_prev()<CR>', "Navigate to previous file" },
-		-- 	["1"] = { '<cmd>lua require("harpoon.ui").nav_file(1)<CR>', "Navigate to file 1" },
-		-- 	["2"] = { '<cmd>lua require("harpoon.ui").nav_file(2)<CR>', "Navigate to file 2" },
-		-- 	["3"] = { '<cmd>lua require("harpoon.ui").nav_file(3)<CR>', "Navigate to file 3" },
-		-- 	["4"] = { '<cmd>lua require("harpoon.ui").nav_file(4)<CR>', "Navigate to file 4" },
-		-- },
-		c = {
-			function()
-				require("Comment.api").toggle.linewise.current()
-			end,
-			"Comment current line",
-		},
-		d = {
-			name = "DAP",
-			t = { '<cmd>lua require("dapui").toggle()<CR>', "Toggle" },
-			b = { '<cmd>lua require("dap").toggle_breakpoint()<CR>', "Toggle breakpoint" },
-			c = { '<cmd>lua require("dap").continue()<CR>', "Continue" },
-			n = { '<cmd>lua require("dap").step_over()<CR>', "Step over" },
-			i = { '<cmd>lua require("dap").step_into()<CR>', "Step into" },
-			r = { '<cmd>lua require("dap").repl.open()<CR>', "Open REPL" },
-		},
-	},
+  ["<leader>"] = {
+    g = {
+      name = "Git",
+      s = { ":G<CR>", "Git status" },
+      c = { ":Git commit<CR>", "Git commit" },
+      b = { ":Telescope git_branches<CR>", "Git branches" },
+    },
+    e = { ":Neotree toggle<CR>", "Toggle file tree" },
+    x = { "<cmd>lua MiniBufremove.delete()<CR>", "Close current buffer" },
+    f = {
+      name = "Files",
+      s = { ":Telescope live_grep<CR>", "Search in files using Telescope" },
+      f = { ":Telescope find_files<CR>", "Search files using Telescope" },
+    },
+    l = {
+      name = "LSP",
+      o = { "<cmd>lua vim.diagnostic.open_float()<CR>", "Open LSP diagnostic window" },
+      q = { "<cmd>lua vim.diagnostic.setloclist()<CR>", "Open loclist using split buffer" },
+      l = { ":Telescope loclist<CR>", "Open loclist using Telescope" },
+      d = { "<cmd>lua vim.lsp.buf.definition()<CR>", "Go to definition" },
+      D = { "<cmd>lua vim.lsp.buf.declaration()<CR>", "Go to declaration" },
+      h = {
+        function()
+          vim.lsp.buf.hover()
+        end,
+        "Toggle LSP hover",
+      },
+      t = { "<cmd>TroubleToggle<CR>", "Toggle Trouble.nvim" }
+    },
+    -- h = {
+    -- 	name = "Harpoon",
+    -- 	m = { '<cmd>lua require("harpoon.mark").add_file()<CR>', "Mark file" },
+    -- 	n = { '<cmd>lua require("harpoon.ui").nav_next()<CR>', "Navigate to next file" },
+    -- 	N = { '<cmd>lua require("harpoon.ui").nav_prev()<CR>', "Navigate to previous file" },
+    -- 	["1"] = { '<cmd>lua require("harpoon.ui").nav_file(1)<CR>', "Navigate to file 1" },
+    -- 	["2"] = { '<cmd>lua require("harpoon.ui").nav_file(2)<CR>', "Navigate to file 2" },
+    -- 	["3"] = { '<cmd>lua require("harpoon.ui").nav_file(3)<CR>', "Navigate to file 3" },
+    -- 	["4"] = { '<cmd>lua require("harpoon.ui").nav_file(4)<CR>', "Navigate to file 4" },
+    -- },
+    c = {
+      function()
+        require("Comment.api").toggle.linewise.current()
+      end,
+      "Comment current line",
+    },
+    d = {
+      name = "DAP",
+      t = { '<cmd>lua require("dapui").toggle()<CR>', "Toggle" },
+      b = { '<cmd>lua require("dap").toggle_breakpoint()<CR>', "Toggle breakpoint" },
+      c = { '<cmd>lua require("dap").continue()<CR>', "Continue" },
+      n = { '<cmd>lua require("dap").step_over()<CR>', "Step over" },
+      i = { '<cmd>lua require("dap").step_into()<CR>', "Step into" },
+      r = { '<cmd>lua require("dap").repl.open()<CR>', "Open REPL" },
+    },
+  },
 })
 
 -- Visual mode mappings
 
 wk.register({
-	["<leader>"] = {
-		c = {
-			"<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
-			"Comment selected block",
-		},
-	},
+  ["<leader>"] = {
+    c = {
+      "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
+      "Comment selected block",
+    },
+  },
 }, { mode = "v" })
 
 -- Navigation improvements
@@ -80,13 +81,13 @@ vim.keymap.set("n", "<C-[>", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
 vim.keymap.set("n", "<C-]>", "<cmd>lua vim.diagnostic.goto_next()<CR>")
 
 function _G.set_terminal_keymaps()
-	local opts = { buffer = 0 }
-	vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-	vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
-	vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-	vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-	vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-	vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+  local opts = { buffer = 0 }
+  vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+  vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+  vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
@@ -99,9 +100,9 @@ set("n", "<Tab>", ":BufferLineCycleNext<CR>", { silent = true, noremap = true })
 -- Autocmd
 --vim.cmd([[autocmd BufWritePre * lua vim.diagnostic.setloclist({open=false})]])
 vim.api.nvim_create_autocmd("BufWritePre", {
-	callback = function()
-		vim.diagnostic.setloclist({ open = false })
-	end,
+  callback = function()
+    vim.diagnostic.setloclist({ open = false })
+  end,
 })
 -- vim.api.nvim_create_autocmd("BufWritePre", {
 -- 	callback = function()
