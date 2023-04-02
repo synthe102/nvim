40d4c3ffb07a1e139595dad7c970bf82d4cdf216
Léonard Suslian (synthe102) <leonard.suslian@gmail.com>
2023-04-02 19:15:24 +0200

    feat(rust-tools): use Toggleterm as executor

--- lua/user/configs/rust_tools.lua
+++ lua/user/configs/rust_tools.lua
@@ -4,28 +4,31 @@ local liblldb_path = path .. "lldb/lib/liblldb.so"
 local opts = {}
 
 local lsp = require("lsp-zero").preset({
-	name = "recommended",
-	set_lsp_keymaps = true,
-	manage_nvim_cmp = true,
-	suggest_lsp_servers = true,
+  name = "recommended",
+  set_lsp_keymaps = true,
+  manage_nvim_cmp = true,
+  suggest_lsp_servers = true,
 })
 
 local rust_lsp = lsp.build_options("rust_analyzer", {})
 
 if vim.fn.filereadable(codelldb_path) and vim.fn.filereadable(liblldb_path) then
-	opts.dap = {
-		adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
-	}
+  opts.dap = {
+    adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+  }
 else
-	local msg = "Either codelldb or liblldb is not readable."
-		.. "\n codelldb: "
-		.. codelldb_path
-		.. "\n liblldb: "
-		.. liblldb_path
-	vim.notify(msg, vim.log.levels.ERROR)
+  local msg = "Either codelldb or liblldb is not readable."
+      .. "\n codelldb: "
+      .. codelldb_path
+      .. "\n liblldb: "
+      .. liblldb_path
+  vim.notify(msg, vim.log.levels.ERROR)
 end
 
 opts.server = rust_lsp
-opts.tools = { hover_actions = { auto_focus = true } }
+opts.tools = {
+  executor = require("rust-tools.executors").toggleterm,
+  hover_actions = { auto_focus = true }
+}
 
 require("rust-tools").setup(opts)
